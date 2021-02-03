73194f8def39e64fc6c95cd3957824593211a034
Vurv <56230599+Vurv78@users.noreply.github.com>
2021-02-02 20:39:15 -0800

    Fix assets + Tidy up files (#2)
    * Use flask's json rather than python's json
    
    * Tidy up file management
    
    Got rid of /site/ which was useless, added a separate Home file route.
    
    Co-authored-by: Vurv78 <Vurv78@users.noreply.github.com>

--- src/libs/JSON.hx
+++ src/libs/JSON.hx
@@ -1,7 +1,7 @@
 package libs;
 import libs.Python;
 
-@:pythonImport("json")
+@:pythonImport("flask","json")
 extern class JSON {
     @:native("dumps") static function encode( tab:PyDict ):String;
     @:native("loads") static function decode( json_str:String ):PyDict;
