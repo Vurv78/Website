6caf9eb2632d3422ddd22e95dce08a7e8ead3fcd
Vurv78 <Vurv78@users.noreply.github.com>
2021-02-12 20:41:40 -0800

    Hopefully fix query?

--- src/libs/Python.hx
+++ src/libs/Python.hx
@@ -2,7 +2,6 @@ package libs;
 using python.Dict;
 
 typedef PyDict = Dict<String, Any>; // Generic Dictionary
-
 @:native("") extern class Externs {
     @:native("__name__") static var py_name: String;
     @:native("globals") static function globals():Dict<Dynamic, Dynamic>;
