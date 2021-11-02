b21b5e76aedced90a0c7c3841c129a97ed5b37d3
Vurv <56230599+Vurv78@users.noreply.github.com>
2021-11-01 19:46:51 -0700

    Fix maybe.
    * Bring back Requests. Haxe's HTTP is really scuffed for python
    * Use raw BytesIO instead of trying to use Haxe's...
    * Add Safety as a dependency

---
+++ src/libs/Requests.hx
@@ -0,0 +1,16 @@
+package libs;
+
+// Haxe's HTTP is trash on python, so..
+
+using python.KwArgs;
+
+@:pythonImport("requests")
+extern class Requests {
+	static function get( url:String, ?args:KwArgs<Dynamic> ):Response;
+	static function post( url:String, ?args:KwArgs<Dynamic> ):Response;
+}
+
+@:pythonImport("requests","response")
+extern class Response {
+	public var content: String;
+}
--- src/routes/Img2Digi.hx
+++ src/routes/Img2Digi.hx
@@ -2,11 +2,10 @@ package routes;
 
 import libs.Flask;
 import libs.Api;
-import python.lib.io.BytesIO;
-import python.Bytes;
+import libs.Requests;
+using Safety;
 
 import python.Tuple;
-import haxe.Http;
 
 final DEFAULT_IMG:String = "https://cdn.discordapp.com/attachments/732861600708690010/799877632737017856/unknown.png";
 
@@ -15,35 +14,30 @@ extern class Image {
 	static function open( data:Any ):Image;
 	function resize( new_size:Tuple<UInt> ):Image; // Returns a copy, doesn't self modify.
 	function getdata():Iterator<Dynamic>; // Returns a 'sequence like' object. Nice python.
-	static function frombytes(mode: String, size: Tuple<UInt>, data: Bytes):Image;
 }
 
-@:nullSafety(Off) // TODO
+// Apparently haxe doesn't directly wrap this BytesIO function. Thanks.
+@:pythonImport("io")
+extern class IO {
+	static function BytesIO( content:String ):Dynamic; // Returns BytesIO Class
+}
+
 function route() {
 	var img_url = Request.args.get("url", DEFAULT_IMG);
 
 	if( !Api.valid_url(img_url) )
 		return Api.punt("Invalid url given.");
 
-	var res = Std.parseInt( Request.args.get("res", "256") );
+	var res = Std.parseInt( Request.args.get("res", "256") ).or(0);
 
 	res = res > 0 ? res : 0; // Don't let res be negative or greater than 512.
 	res = res > 512 ? 512 : res;
 
 	var version = Std.parseInt( Request.args.get("version","1") );
 
-	var out = "";
-	var response = new Http(img_url);
-		response.setParameter("timeout", "3");
-		response.onData = function(data) {
-			out = data;
-		}
-		response.onError = function(e) {
-			return Api.punt(e);
-		}
-		response.request(false);
+	final response = Requests.get(img_url, {"timeout": 3});
 
-	var img = Image.open( new BytesIO( untyped new Bytes(out, "ascii") ) );
+	var img = Image.open( IO.BytesIO( response.content ) );
 	img = img.resize( new python.Tuple( [res, res] ) );
 
 	return switch version {
